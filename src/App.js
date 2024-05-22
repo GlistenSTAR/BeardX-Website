20fc35f235249ff59243fccb9bde4f0d30ec658a
GlistenSTAR <glistenstar00@gmail.com>
2024-05-22 08:00:50 +0200

    update: fix the router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Index from './components/Index';
 import Stacking from './components/stacking';
@@ -7,7 +7,7 @@ import Footer from './components/layouts/Footer';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className='App'>
         <Header />
         <Routes>
